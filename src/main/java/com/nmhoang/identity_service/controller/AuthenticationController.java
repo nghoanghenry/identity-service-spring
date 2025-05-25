aedbc1614dc7687d607f59513997a154ddf7e303
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-05-25 14:55:50 +0700

    Update Spring Security

--- src/main/java/com/nmhoang/identity_service/controller/AuthenticationController.java
+++ src/main/java/com/nmhoang/identity_service/controller/AuthenticationController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 
 @RestController
-@RequestMapping("/authen")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationController {
