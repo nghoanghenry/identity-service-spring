02c706c680bb1bd22dc00f4910f6954829c53014
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-05-29 11:32:25 +0700

    Add custom exception in Spring security

--- src/main/java/com/nmhoang/identity_service/exception/ErrorCode.java
+++ src/main/java/com/nmhoang/identity_service/exception/ErrorCode.java
@@ -1,34 +1,38 @@
 package com.nmhoang.identity_service.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    USER_EXISTED(1001, "User existed"),
-    USER_NOT_EXISTED(1002, "User not existed"),
-    USERNAME_NOT_VALID(1003, "The username must be at least 4 characters"),
-    PASSWORD_NOT_VALID(1004, "The password must be at least 8 characters"),
-    INCORRECT_PASSWORD(1005, "Incorrect password"),
-    INTERNAL_ERROR(1006, "Internal error"),
+    USER_EXISTED(1001, "User existed", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1002, "User not existed", HttpStatus.NOT_FOUND),
+    USERNAME_NOT_VALID(1003, "The username must be at least 4 characters", HttpStatus.BAD_REQUEST),
+    PASSWORD_NOT_VALID(1004, "The password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    INCORRECT_PASSWORD(1005, "Incorrect password", HttpStatus.BAD_REQUEST),
+    INTERNAL_ERROR(1006, "Internal error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNAUTHENTICATION(1007, "Unauthorized", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1008, "Unauthorized client", HttpStatus.UNAUTHORIZED),
     ;
     private int code;
     private String message;
+    private HttpStatusCode httpStatusCode;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode httpStatusCode) {
         this.code = code;
         this.message = message;
+        this.httpStatusCode = httpStatusCode;
     }
 
-    public int getCode() {
-        return code;
-    }
 
     public void setCode(int code) {
         this.code = code;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public void setHttpStatusCode(HttpStatusCode httpStatusCode) {}
 }
--- src/main/java/com/nmhoang/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nmhoang/identity_service/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.nmhoang.identity_service.exception;
 
 import com.nmhoang.identity_service.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,7 +25,21 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = e.getErrorCode();
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity
+                .status(errorCode.getHttpStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException e) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatusCode())
+                .body(apiResponse);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
